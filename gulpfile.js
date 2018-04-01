58bf5824ef97be681937f68a6d393314fef49e61
yutanoma <yn.devilstick@gmail.com>
2018-04-01 14:16:56 +0900

    made: repository
    Signed-off-by: yutanoma <yn.devilstick@gmail.com>

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var browserSync = require("browser-sync");
+var notify = require("gulp-notify");
+var pug = require("gulp-pug");
+
+gulp.task('default', ['sass', 'browser-sync', 'pug', 'watch']);
+
+//sassとpugの監視をして変換処理させる
+gulp.task('watch', () => {
+	gulp.watch(['./sass/**'], () => {
+		gulp.start(['sass']);
+	});
+	gulp.watch(['./pug/**'], () => {
+		gulp.start(['pug']);
+	});
+});
+
+//ブラウザ表示
+gulp.task('browser-sync', () => {
+	browserSync({
+		server: {
+			baseDir: "./" //サーバとなるrootディレクトリ
+		}
+	});
+	//ファイルの監視
+	//以下のファイルが変わったらリロードする
+	gulp.watch("./js/**/*.js", ['reload']);
+	gulp.watch("./*.html", ['reload']);
+});
+
+//sassをcssに変換
+gulp.task("sass", () => {
+	gulp.src("./sass/**/*scss")
+		.pipe(plumber({
+			errorHandler: notify.onError("Error: <%= error.message %>")
+		}))
+		.pipe(sass())
+		.pipe(gulp.dest("./css"))
+		//reloadせずにinjectする
+		.pipe(browserSync.stream())
+});
+
+//pugをhtmlに変換
+gulp.task("pug", () => {
+	var option = {
+		pretty: true
+	}
+	gulp.src("./pug/**/*.pug")
+		.pipe(plumber({
+			errorHandler: notify.onError("Error: <%= error.message %>")
+		}))
+		.pipe(pug(option))
+		.pipe(gulp.dest("./"))
+});
+
+//ブラウザリロード処理
+gulp.task('reload', () => {
+	browserSync.reload();
+});
